348da7ba33bed652efab844595bdaae2a161559b
Dmitry Vishniakov <112121dfd@gmail.com>
2023-05-17 16:03:05 +0700

    Change pom.xml and add jacoco

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class StatisticServiceTest {
 
     @Test
-    void findMax() {
+    public void findMax() {
         StatisticService service = new StatisticService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -15,5 +15,19 @@ public class StatisticServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxIfZeroIndexLess() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillion = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillion);
+
+        Assertions.assertEquals(expected, actual);
+
     }
 }
